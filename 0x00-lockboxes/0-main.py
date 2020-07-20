2909ba3dbfdd908527b30836d21d0209f174b7dc
Anthony <huggins9000211@gmail.com>
2020-07-20 03:27:23 -0400

    test

--- 0x00-lockboxes/0-main.py
+++ 0x00-lockboxes/0-main.py
@@ -1,12 +1,75 @@
 #!/usr/bin/python3
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
-
 boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+boxes = [[1, 4, 6], [2, 0], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+print(canUnlockAll(boxes),  "\t: False")
+print('-----------------------------')
+boxes = [[1,3],[3,0,1],[2],[0]]
+print(canUnlockAll(boxes),  "\t: False")
+print('-----------------------------')
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6], [7]]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6], [7]]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+boxes = [[2], [3, 4], [1, 5]]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+boxes = [[2]]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+# boxes = []
+# print(canUnlockAll(boxes), "\t: False")
+# print('-----------------------------')
+boxes = [[1, 2, 3], [], [], [], []]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+# boxes = None
+# print(canUnlockAll(boxes), "\t: False")
+# print('-----------------------------')
+# boxes = 'hello, is it me you looking for?'
+# print(canUnlockAll(boxes), "\t: False")
+# print('-----------------------------')
+boxes = [[0, 4, 4], [5], [3], [1], [2], [5]]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+boxes = [[1, 4], [2], [0, 4, 1], [6], [3], [4, 1], [5, 6], [7]]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+boxes = [[1], [1], [1]]
+print(canUnlockAll(boxes), "\t: False 6")
+print('-----------------------------')
+boxes = [[], [2], [4]]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+boxes = [[0, 1], [2, 3], {}, None]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+boxes = [[]]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+boxes = [[1, 2], ['hey', 'you'], ['sup']]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+boxes = [{}, [2, 3], {}, None]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+boxes = [[0, 1], [2, 3], [7, 4], [2]]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+boxes = [{}]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
+boxes = [[2]]
+print(canUnlockAll(boxes), "\t: True")
+print('-----------------------------')
+boxes = [[], [1, 2, 7], [0, 3]]
+print(canUnlockAll(boxes), "\t: False")
+print('-----------------------------')
