50f6f535c105b64f752fe8ab4334b44680d72a1e
Anthony <huggins9000211@gmail.com>
2020-07-28 11:43:03 -0400

    test

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - check the code for Holberton School students.
+ * @head: head
+ * @number: number
+ *
+ * Return: Always 0.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	listint_t *first = *head;
+
+	if (!first)
+	{
+		new->next = NULL;
+		first = new;
+		return (new);
+	}
+
+	while (first->next->n < number)
+	{
+		first = first->next;
+	}
+	new->next = first->next;
+	first->next = new;
+	return (new);
+}
