3c48099a71a2829c7cb4db090b4e67e4a4a153f4
Anthony <huggins9000211@gmail.com>
2020-08-02 12:22:10 -0400

    test

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -2,30 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Entry point
- *
- * @tree: tree
- *
- * Return: Always 0 (Success)
- */
-/*
-*size_t binary_tree_nodes(const binary_tree_t *tree)
-*{
-	*int l;
-*
-	*if (!tree)
-	*	return (0);
-	*if (!binary_tree_is_leaf(tree))
-*		l = 1;
-*	else
-*		l = 0;
-*	l = l + binary_tree_nodes(tree->left);
-*	l = l + binary_tree_nodes(tree->right);
-*	return (l);
-*}
-*/
-/**
- * binary_tree_node - Measures the height of a binary tree
+ * heap_insert - Measures the height of a binary tree
  *
  * @root: Pointer to the node to measures the height
  * @value: Pointer to the node to measures the height
