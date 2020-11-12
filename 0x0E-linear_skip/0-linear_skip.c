c9e2d6993284d500eff84520306f4792a2c527a1
Anthony <huggins9000211@gmail.com>
2020-11-11 22:06:01 -0500

    test

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -3,16 +3,22 @@
 #include <math.h>
 
 #include "search.h"
- 
 
+/**
+* printN - Initializes the express lane of the linked list
+*
+* @node: Pointer to the head node of the list
+*
+* Return: A pointer to the head of the created list (NULL on failure)
+*/
 int printN(skiplist_t *node)
 {
-   printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-   return (node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	return (node->n);
 }
 
 /**
-* init_express - Initializes the express lane of the linked list
+* linear_skip - Initializes the express lane of the linked list
 *
 * @list: Pointer to the head node of the list
 * @value: Number of nodes in the list
@@ -21,55 +27,43 @@ int printN(skiplist_t *node)
 */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-   skiplist_t *node;
-   skiplist_t *copy;
-   skiplist_t *lastExp;
-   int len;
+	skiplist_t *node, *copy, *lastExp;
+	int len;
 
-   len = 0;
-   copy = list;
-   lastExp = list;
-   node = list;
-   while(node)
-   {
-       len++;
-       node = node->next;
-   }
-   if (!list)
-   {
-       return (NULL);
-   }
-   while (copy->express)
-   {
-       copy = copy->express;
-       if (value <= printN(copy))
-       {
-           
-           printf("Value found between indexes [%ld] and [%ld]\n", lastExp->index, copy->index);
-           while (lastExp->index <= copy->index)
-           {
-               if ( value == printN(lastExp))
-               {
-                   return (lastExp);
-               }
-               lastExp = lastExp->next;
-           }
-           return (NULL);
-       }
-       lastExp = copy;
-   }
-   printf("Value found between indexes [%ld] and [%d]\n", copy->index, len);
-   while (copy)
-   {
-       if (value == printN(copy))
-       {
-           return (copy);
-       }
-       copy = copy->next;
-   }
-   
-
-   return (NULL);
+	len = 0;
+	copy = list;
+	lastExp = list;
+	node = list;
+	while (node)
+	{
+		len++;
+		node = node->next;
+	}
+	if (!list)
+	return (NULL);
+	while (copy->express)
+	{
+		copy = copy->express;
+		if (value <= printN(copy))
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n"
+			, lastExp->index, copy->index);
+			while (lastExp->index <= copy->index)
+			{
+				if (value == printN(lastExp))
+				return (lastExp);
+				lastExp = lastExp->next;
+			}
+			return (NULL);
+		}
+		lastExp = copy;
+	}
+	printf("Value found between indexes [%ld] and [%d]\n", copy->index, len);
+	while (copy)
+	{
+		if (value == printN(copy))
+		return (copy);
+		copy = copy->next;
+	}
+	return (NULL);
 }
- 
-
