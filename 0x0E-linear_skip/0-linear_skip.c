683de637fd7e4864f958b75257b232f530198d6f
Anthony <huggins9000211@gmail.com>
2020-11-11 21:55:58 -0500

    test

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "search.h"
+ 
+
+int printN(skiplist_t *node)
+{
+   printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+   return (node->n);
+}
+
+/**
+* init_express - Initializes the express lane of the linked list
+*
+* @list: Pointer to the head node of the list
+* @value: Number of nodes in the list
+*
+* Return: A pointer to the head of the created list (NULL on failure)
+*/
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+   skiplist_t *node;
+   skiplist_t *copy;
+   skiplist_t *lastExp;
+   int len;
+
+   len = 0;
+   copy = list;
+   lastExp = list;
+   node = list;
+   while(node)
+   {
+       len++;
+       node = node->next;
+   }
+   if (!list)
+   {
+       return (NULL);
+   }
+   while (copy->express)
+   {
+       copy = copy->express;
+       if (value <= printN(copy))
+       {
+           
+           printf("Value found between indexes [%ld] and [%ld]\n", lastExp->index, copy->index);
+           while (lastExp->index <= copy->index)
+           {
+               if ( value == printN(lastExp))
+               {
+                   return (lastExp);
+               }
+               lastExp = lastExp->next;
+           }
+           return (NULL);
+       }
+       lastExp = copy;
+   }
+   printf("Value found between indexes [%ld] and [%d]\n", copy->index, len);
+   while (copy)
+   {
+       if (value == printN(copy))
+       {
+           return (copy);
+       }
+       copy = copy->next;
+   }
+   
+
+   return (NULL);
+}
+ 
+
