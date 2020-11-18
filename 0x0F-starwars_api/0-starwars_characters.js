4d85feb932acd08f283ba3b97a8879ff1aaebd36
Anthony <huggins9000211@gmail.com>
2020-11-18 11:43:55 -0500

    test

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        console.log(JSON.parse(body).name);
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+var chars;
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, async function (error, response, body) {
+  if (error) {
+    // console.error('error:', error);
+  }
+  chars = JSON.parse(body).characters;
+  var x;
+  for (x of chars) {
+    await doRequest(x);
+    // console.log(JSON.parse(res.body).name);
+  }
+});
