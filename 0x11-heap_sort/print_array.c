14fb8115e15c50826e09deea725dfcf0bc55d76d
Anthony <huggins9000211@gmail.com>
2020-12-02 23:34:54 -0500

    test

---
+++ 0x11-heap_sort/print_array.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
