0d0b01913c9580ed5db93f814c6c9f22f67f1ebd
Anthony <huggins9000211@gmail.com>
2020-12-09 17:51:49 -0500

    test

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+	if ((r - l == 0) && (arr[r] != x))
+		return (-1);
+	if (r >= l)
+	{
+		int i;
+		int mid;
+
+		i = l;
+		printf("Searching in array: ");
+		while (i <= r)
+		{
+			if (i == r)
+				printf("%d\n", arr[r]);
+			else
+				printf("%d, ", arr[i]);
+			i++;
+		}
+		mid = l + (r - l) / 2;
+
+		if (arr[mid] == x)
+		{
+			if (arr[mid - 1] == x)
+			{
+				r = mid;
+				l = mid;
+				while (arr[l - 1] == x)
+					l--;
+				return (binarySearch(arr, l, r, x));
+			}
+			return (mid);
+		}
+		if (arr[mid] > x)
+			return (binarySearch(arr, l, mid - 1, x));
+		return (binarySearch(arr, mid + 1, r, x));
+	}
+	return (-1);
+}
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int result;
+
+	if (!array)
+		return (-1);
+	result = binarySearch(array, 0, size - 1, value);
+	return (result);
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int r, int x);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* LISTS_H */
