790e15c9a4923e6aba6f76e402c7aeccfb7a4e46
Anthony <huggins9000211@gmail.com>
2021-04-15 17:51:57 -0400

    test

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def rain(walls):
+    ans = 0
+    size = len(walls)
+    leftMax = [None] * size
+    rightMax = [None] * size
+    leftMax[0] = walls[0]
+    i = 1
+    while i < size:
+        leftMax[i] = max([walls[i], leftMax[i - 1]])
+        i += 1
+    rightMax[size - 1] = walls[size - 1]
+    i = size - 2
+    while i >= 0:
+        rightMax[i] = max([walls[i], rightMax[i + 1]])
+        i -= 1
+    i = 1
+    while i < size - 1:
+        ans += min([leftMax[i], rightMax[i]]) - walls[i]
+        i += 1
+    return ans
