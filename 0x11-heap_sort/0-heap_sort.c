4276bdfb06de3934346a7bd6084404cf5ff35317
Anthony <huggins9000211@gmail.com>
2021-04-22 14:58:19 -0400

    test

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -42,8 +42,12 @@ void heap_sort(int *array, size_t size)
     for (i = size - 1; i >= 0; i--)
     {
         swap(&array[0], &array[i]);
-        print_array(array, size);
+        if (i > 0)
+        {
+            print_array(array, size);
+        }
         heapify(array, i, 0, size);
+        
     }
 }
 
