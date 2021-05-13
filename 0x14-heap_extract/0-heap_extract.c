ae57518bdc764e6b61a551efb669a7297e2a89e8
Anthony <huggins9000211@gmail.com>
2021-05-12 21:04:41 -0700

    test

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * fixHeap - test
+ * @node: test
+ */
+void fixHeap(binary_tree_t *node)
+{
+	binary_tree_t *largest = NULL;
+	int temp;
+
+	if (node->right)
+	{
+		if (node->right->n > node->left->n)
+			largest = node->right;
+		else
+			largest = node->left;
+	}
+	else if (node->left)
+		largest = node->left;
+
+	if (largest && largest->n > node->n)
+	{
+		temp = largest->n;
+		largest->n = node->n;
+		node->n = temp;
+		fixHeap(largest);
+	}
+}
+/**
+ * getHeight - test
+ * @node: test
+ * Return: test
+ */
+int getHeight(binary_tree_t *node)
+{
+	int l, r;
+
+	if (!node)
+		return (0);
+	l = getHeight(node->left);
+	r = getHeight(node->right);
+	if (l > r)
+		return (l + 1);
+	return (r + 1);
+}
+/**
+ * getLast - test
+ * @height: test
+ * @helper: test
+ * @node: test
+ * Return: test
+ */
+binary_tree_t *getLast(int height, int helper, binary_tree_t *node)
+{
+	binary_tree_t *left;
+	binary_tree_t *right;
+
+	if (helper != height)
+	{
+		left = getLast(height, helper + 1, node->left);
+		right = getLast(height, helper + 1, node->right);
+	}
+	else
+		return (node);
+	if (right)
+		return (right);
+	return (left);
+}
+/**
+ * heap_extract - test
+ * @root: test
+ * Return: test
+ */
+int heap_extract(binary_tree_t **root)
+{
+	binary_tree_t *last;
+	int deleted;
+    int height;
+
+	if (!*root)
+		return (0);
+
+	if (!(*root)->left && !(*root)->right)
+	{
+		deleted = (*root)->n;
+		free(*root);
+		*root = NULL;
+		return (deleted);
+	}
+    height = getHeight(*root);
+	last = getLast(height, 1, *root)
+	if (last->parent->right)
+		last->parent->right = NULL;
+	else
+		last->parent->left = NULL;
+	deleted = (*root)->n;
+	(*root)->n = last->n;
+	fixHeap(*root);
+	free(last);
+	return (deleted);
+}
---
+++ 0x14-heap_extract/binary_trees.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+int heap_extract(heap_t **root);
+
+#endif /* LISTS_H */
